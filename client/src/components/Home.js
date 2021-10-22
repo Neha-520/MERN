9dfc38505becf0e8852368ccad4da0caad381d3f
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-10-22 14:08:55 +0530

    Front end with react setup

---
+++ client/src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Home = () => {
+    return (
+        <div>
+            <p>WELCOME</p>
+            <h1>We Are The MERN Developer</h1>
+        </div>
+    )
+}
