1e2525f740d53d2b78d9c7543903c95ec349bddf
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-11-14 22:29:07 +0530

    get form data

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 export const Signup = () => {
+
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        work: "",
+        password: "",
+        cpassword: "",
+    });
+
+    const handleInputs = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value })
+    }
+
     return (
         <>
             <div className="x">
@@ -14,37 +28,55 @@ export const Signup = () => {
                                     <div className="form-group">
                                         <label htmlFor="name">
                                             <i class="zmdi zmdi-account material-icons-name"></i>                                    </label>
-                                        <input type="text" name="name" id="name" autoComplete="off" placeholder="Your Name"></input>
+                                        <input type="text" name="name" id="name" autoComplete="off"
+                                            value={user.name}
+                                            onChange={handleInputs}
+                                            placeholder="Your Name"></input>
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="email">
                                             <i className="zmdi zmdi-email material-icons-name"></i>
                                         </label>
-                                        <input type="email" name="email" id="email" autoComplete="off" placeholder="Your Email"></input>
+                                        <input type="email" name="email" id="email" autoComplete="off"
+                                            value={user.email}
+                                            onChange={handleInputs}
+                                            placeholder="Your Email"></input>
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="phone">
                                             <i className="zmdi zmdi-phone-in-talk material-icons-name"></i>
                                         </label>
-                                        <input type="number" name="phone" id="phone" autoComplete="off" placeholder="Your Phone Number"></input>
+                                        <input type="number" name="phone" id="phone" autoComplete="off"
+                                            value={user.phone}
+                                            onChange={handleInputs}
+                                            placeholder="Your Phone Number"></input>
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="work">
                                             <i className="zmdi zmdi-slideshow material-icons-name"></i>
                                         </label>
-                                        <input type="text" name="work" id="work" autoComplete="off" placeholder="Your Profession"></input>
+                                        <input type="text" name="work" id="work" autoComplete="off"
+                                            value={user.work}
+                                            onChange={handleInputs}
+                                            placeholder="Your Profession"></input>
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="password">
                                             <i className="zmdi zmdi-lock material-icons-name"></i>
                                         </label>
-                                        <input type="password" name="password" id="password" autoComplete="off" placeholder="Your Password"></input>
+                                        <input type="password" name="password" id="password" autoComplete="off"
+                                            value={user.password}
+                                            onChange={handleInputs}
+                                            placeholder="Your Password"></input>
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="cpassword">
                                             <i className="zmdi zmdi-lock material-icons-name"></i>
                                         </label>
-                                        <input type="password" name="cpassword" id="cpassword" autoComplete="off" placeholder="Confirm Your Password"></input>
+                                        <input type="password" name="cpassword" id="cpassword" autoComplete="off"
+                                            value={user.cpassword}
+                                            onChange={handleInputs}
+                                            placeholder="Confirm Your Password"></input>
                                     </div>
                                     <div className="form-group form-button mt-5" >
                                         <input type="submit" name="signup" id="signup" className="form-submit" value="Register"></input>
@@ -53,7 +85,7 @@ export const Signup = () => {
                             </div>
                             <div className="signup-image mt-2  col-6"  >
                                 <figure >
-                                    <img src="/register.jpg" alt="registration pic" style={{ width: "280px", height: "300px", objectFit: "contain" }}></img>
+                                    <img src="/register.jpg" alt="registration pic" style={{ width: "250px", height: "300px", objectFit: "fit-content" }}></img>
                                 </figure>
                                 <NavLink to="/login" className="signup-image-link">I am already registered</NavLink>
                             </div>
